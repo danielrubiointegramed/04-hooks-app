09841588392b6085ab63c597058b0eb250fb55b7
danielrubiointegramed <daniel.rubio.integramed@gmail.com>
2025-09-29 11:58:28 -0600

    Continuación

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 //import { HooksApp } from './HooksApp';
 //import { TrafficLight } from './01-useState/TrafficLight';
-import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffct';
+//import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <TrafficLight /> */}
     {/* <HooksApp /> */}
-    <TrafficLightWithEffect />
+    {/* <TrafficLightWithEffect /> */}
+    <TrafficLightWithHook />
   </StrictMode>,
 )
